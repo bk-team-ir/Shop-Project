49a62dfd7adc7906f2827574bdec3488a8639b2b
AHSharifi <mr.amirhossein88@gmail.com>
2024-12-11 15:50:20 +0330

    add [ database ]

--- database/database.js
+++ database/database.js
@@ -4,3 +4,8 @@ const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlycXBsbWtma3lhd296ZWFmcGFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM4NTI5NjIsImV4cCI6MjA0OTQyODk2Mn0.RwfwQcEtDPWtCBUfq32XJkgrt9ADz7acrxT6hjBuT_E";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+export default supabase
+
+
+
+
