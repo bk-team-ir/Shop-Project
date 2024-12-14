73b01ac65007179db827b4b742334c8316393bb1
AHSharifi <mr.amirhossein88@gmail.com>
2024-12-15 00:28:58 +0330

    add [ blogDetails ]

--- js/blog.js
+++ js/blog.js
@@ -5,13 +5,15 @@ async function loadBlogs() {
   let { data: blogs } = await supabase.from("blogs").select("*");
   blogs.forEach((blog) => {
     const box = `
-    <div class="box">
-        <img
-          src="${blog.image}"
-          alt="${blog.title}"
-        />
-        <h1>${blog.title}</h1>
-        <p>${blog.text}</p>
+      <div class="box">
+        <a href="blogDetails.html?id=${blog.id}">
+          <img
+            src="${blog.image}"
+            alt="${blog.title}"
+          />
+          <h1>${blog.title}</h1>
+          <p>${blog.text}</p>
+        </a>
       </div>
     `;
     blogsBox.innerHTML += box;
---
+++ js/blogDetails.js
@@ -0,0 +1,27 @@
+import supabase from "/database/database.js"
+
+const urlParams = new URLSearchParams(window.location.search);
+const blogId = urlParams.get("id");
+
+async function loadBlogDetails() {
+  let { data: blog } = await supabase
+    .from("blogs")
+    .select("*")
+    .eq("id", blogId)
+    .single();
+
+  if (blog) {
+    const box = `
+      <div class="title">
+        <h1>${blog.title}</h1>
+      </div>
+      <img src="${blog.image}" alt="${blog.name}" />
+      <p>${blog.text}</p>
+    `;
+    document.getElementById("blog").innerHTML = box
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadBlogDetails();
+});
